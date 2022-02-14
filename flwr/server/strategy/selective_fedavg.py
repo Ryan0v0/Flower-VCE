fcea9df044bd534d554855755d56e6faa405ed92
Ryan0v0 <sxxhgz@163.com>
2022-02-14 17:57:55 +0100

    creative selective_fedavg deriving from fedavg

---
+++ flwr/server/strategy/selective_fedavg.py
@@ -0,0 +1,143 @@
+# Copyright 2020 Adap GmbH. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ==============================================================================
+"""Federated Averaging (FedAvg) [McMahan et al., 2016] strategy.
+
+Paper: https://arxiv.org/abs/1602.05629
+"""
+
+
+from logging import WARNING
+from typing import Callable, Dict, List, Optional, Tuple
+
+from flwr.common import (
+    EvaluateIns,
+    EvaluateRes,
+    FitIns,
+    FitRes,
+    Parameters,
+    Scalar,
+    Weights,
+    parameters_to_weights,
+    weights_to_parameters,
+)
+from flwr.common.logger import log
+from flwr.server.client_manager import ClientManager
+from flwr.server.client_proxy import ClientProxy
+
+from .aggregate import aggregate, weighted_loss_avg
+from .strategy import Strategy
+from .FedAvg import FedAvg
+
+DEPRECATION_WARNING = """
+DEPRECATION WARNING: deprecated `eval_fn` return format
+
+    loss, accuracy
+
+move to
+
+    loss, {"accuracy": accuracy}
+
+instead. Note that compatibility with the deprecated return format will be
+removed in a future release.
+"""
+
+DEPRECATION_WARNING_INITIAL_PARAMETERS = """
+DEPRECATION WARNING: deprecated initial parameter type
+
+    flwr.common.Weights (i.e., List[np.ndarray])
+
+will be removed in a future update, move to
+
+    flwr.common.Parameters
+
+instead. Use
+
+    parameters = flwr.common.weights_to_parameters(weights)
+
+to easily transform `Weights` to `Parameters`.
+"""
+
+WARNING_MIN_AVAILABLE_CLIENTS_TOO_LOW = """
+Setting `min_available_clients` lower than `min_fit_clients` or
+`min_eval_clients` can cause the server to fail when there are too few clients
+connected to the server. `min_available_clients` must be set to a value larger
+than or equal to the values of `min_fit_clients` and `min_eval_clients`.
+"""
+
+
+class Selective_FedAvg(FedAvg):
+    """Configurable FedAvg strategy implementation."""
+
+    # pylint: disable=too-many-arguments,too-many-instance-attributes
+    def __init__(
+        self,
+        fraction_fit: float = 0.1,
+        fraction_eval: float = 0.1,
+        min_fit_clients: int = 2,
+        min_eval_clients: int = 2,
+        min_available_clients: int = 2,
+        eval_fn: Optional[
+            Callable[[Weights], Optional[Tuple[float, Dict[str, Scalar]]]]
+        ] = None,
+        on_fit_config_fn: Optional[Callable[[int], Dict[str, Scalar]]] = None,
+        on_evaluate_config_fn: Optional[Callable[[int], Dict[str, Scalar]]] = None,
+        accept_failures: bool = True,
+        initial_parameters: Optional[Parameters] = None,
+    ) -> None:
+        """Federated Averaging strategy.
+
+        Implementation based on https://arxiv.org/abs/1602.05629
+
+        Parameters
+        ----------
+        fraction_fit : float, optional
+            Fraction of clients used during training. Defaults to 0.1.
+        fraction_eval : float, optional
+            Fraction of clients used during validation. Defaults to 0.1.
+        min_fit_clients : int, optional
+            Minimum number of clients used during training. Defaults to 2.
+        min_eval_clients : int, optional
+            Minimum number of clients used during validation. Defaults to 2.
+        min_available_clients : int, optional
+            Minimum number of total clients in the system. Defaults to 2.
+        eval_fn : Callable[[Weights], Optional[Tuple[float, Dict[str, Scalar]]]]
+            Optional function used for validation. Defaults to None.
+        on_fit_config_fn : Callable[[int], Dict[str, Scalar]], optional
+            Function used to configure training. Defaults to None.
+        on_evaluate_config_fn : Callable[[int], Dict[str, Scalar]], optional
+            Function used to configure validation. Defaults to None.
+        accept_failures : bool, optional
+            Whether or not accept rounds containing failures. Defaults to True.
+        initial_parameters : Parameters, optional
+            Initial global model parameters.
+        """
+        super().__init__()
+
+        if (
+            min_fit_clients > min_available_clients
+            or min_eval_clients > min_available_clients
+        ):
+            log(WARNING, WARNING_MIN_AVAILABLE_CLIENTS_TOO_LOW)
+
+        self.fraction_fit = fraction_fit
+        self.fraction_eval = fraction_eval
+        self.min_fit_clients = min_fit_clients
+        self.min_eval_clients = min_eval_clients
+        self.min_available_clients = min_available_clients
+        self.eval_fn = eval_fn
+        self.on_fit_config_fn = on_fit_config_fn
+        self.on_evaluate_config_fn = on_evaluate_config_fn
+        self.accept_failures = accept_failures
+        self.initial_parameters = initial_parameters
